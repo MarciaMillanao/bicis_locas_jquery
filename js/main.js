4f3fcafaa1ea9c03594bb7a856464df80374b2f8
marcia <marcia.millanao@gmail.com>
2016-11-16 11:31:05 -0300

    actualizacion de mensajes

--- js/main.js
+++ js/main.js
@@ -3,30 +3,30 @@ function validateForm(){
 	var soloLetras = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
 
 	function name(){//valida nombre
-		var nombre =  $("#name").val(); //document.getElementById('name').value;
+		var nombre = $("#name").val(); //document.getElementById('name').value;
 		if(nombre.length === 0 || nombre === null){
-			var agregar = document.getElementsByClassName('name-container')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#name").parent().append("<span>Ingresar Nombre</span>"); //document.getElementsByClassName('name-container')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Debe ingresar su nombre'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("ingrese nombre");
+			//alert("ingrese nombre");*/
 			return false;
 		}else if(!soloLetras.test(nombre)){
-			var agregar = document.getElementsByClassName('name-container')[0]; //llamo al elemento al que quiero afectar
+			var agregar = $("#name").parent().append("<span>Solo Letras</span>")/*document.getElementsByClassName('name-container')[0]; //llamo al elemento al que quiero afectar
 			var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Ingrese solo letras en el nombre'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("ingrese solo letras");
+			//alert("ingrese solo letras");*/
 			return false;
 		}else if(nombre.substring(0,1) !== nombre.substring(0,1).toUpperCase()){
-			var agregar = document.getElementsByClassName('name-container')[0]; //llamo al elemento al que quiero afectar
+			var agregar = $("#name").parent().append("<span>Inicial con Mayuscula</span>")/*document.getElementsByClassName('name-container')[0]; //llamo al elemento al que quiero afectar
 			var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Ingrese inicial de nombre con mayuscula'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("ingrese inicial con mayuscula");
+			//alert("ingrese inicial con mayuscula");*/
 			return false;
 		}else{
 			//alert("nombre correcto");
@@ -39,28 +39,28 @@ function validateForm(){
 	function apellido(){
 		var apellido = $("#lastname").val(); //document.getElementById('lastname').value;
 		if(apellido.length === 0 || apellido === null){ //valida que se ingrese correo
-			var agregar = document.getElementsByClassName('lastname-container')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#lastname").parent().append("<span>Ingresar Apellido</span>"); //document.getElementsByClassName('lastname-container')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Debe ingresar su apellido'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("Ingrese Apellido");
+			//alert("Ingrese Apellido");*/
 			return false;
 		}else if (!soloLetras.test(apellido)){
-			var agregar = document.getElementsByClassName('lastname-container')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#lastname").parent().append("<span>Solo Letras</span>"); //document.getElementsByClassName('lastname-container')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Ingrese solo letras en el apellido'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("ingrese solo letras");
+			//alert("ingrese solo letras");*/
 			return false;	
 		}else if(apellido.substring(0,1) !== apellido.substring(0,1).toUpperCase()){
-			var agregar = document.getElementsByClassName('lastname-container')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#lastname").parent().append("<span>Inicial con Mayuscula</span>"); //document.getElementsByClassName('lastname-container')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Ingrese inicial de apellido con mayuscula'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("ingrese inicial con mayuscula");
+			//alert("ingrese inicial con mayuscula");*/
 			return false;
 		}else{
 			//alert(apellido ingresado correctamente);
@@ -73,20 +73,20 @@ function validateForm(){
 	function correo(){
 		var email = $("#input-email").val(); //document.getElementById('input-email').value;
 		if(email.length === 0 || email === null){ //valida que se ingrese correo
-			var agregar = document.getElementsByClassName('email-container')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#input-email").parent().append("<span>Ingrese Correo</span>"); //document.getElementsByClassName('email-container')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Debe ingresar su email'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("Ingrese Email");
+			//alert("Ingrese Email");*/
 			return false;
 		}else if (!emailValido.test(email)){
-			var agregar = document.getElementsByClassName('email-container')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#input-email").parent().append("<span>Correo invalido</span>"); //document.getElementsByClassName('email-container')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('correo invalida'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert ("correo invalido");
+			//alert ("correo invalido");*/
 			return false;
 		}
 	}
@@ -96,28 +96,28 @@ function validateForm(){
 	function contrasena(){
 		var password = $("#input-password").val(); //document.getElementById('input-password').value;
 		if(password.length === 0 || password === null){ //valida que se ingrese correo
-			var agregar = document.getElementsByClassName('form-group')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#input-password").parent().append("<span>Ingrese contraseña</span>"); //document.getElementsByClassName('form-group')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Ingrese Password'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("Ingrese Password");
+			//alert("Ingrese Password");*/
 			return false;
 		}else if(password === "123456" || password === "098754" || password === "password"){
-			var agregar = document.getElementsByClassName('form-group')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#input-password").parent().append("<span>No puede ser 123456 o 098764 o password</span>"); //document.getElementsByClassName('form-group')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('Tu contraseña no puede ser 123456 o 098764 o password'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("Tu contraseña no puede ser 123456 o 098764 o password");
+			//alert("Tu contraseña no puede ser 123456 o 098764 o password");*/
 			return false;
 		}else if(password.length<6){
-			var agregar = document.getElementsByClassName('form-group')[0]; //llamo al elemento al que quiero afectar
-			var elementoSpan = document.createElement('span'); //creo span
+			var agregar = $("#input-password").parent().append("<span>Debe tener mas de 6 caracteres</span>"); //document.getElementsByClassName('form-group')[0]; //llamo al elemento al que quiero afectar
+			/*var elementoSpan = document.createElement('span'); //creo span
 			var nodoTexto = document.createTextNode('tu contraseña debe tener mas de 6 caracteres'); //creo nodo texto
 			elementoSpan.appendChild(nodoTexto); //le digo a elemento elementoSpan que nodoTexto es su hijo
 			agregar.appendChild(elementoSpan); //le digo  agregar que elementoSpan es su hijo
-			//alert("tu contraseña debe tener mas de 6 caracteres");
+			//alert("tu contraseña debe tener mas de 6 caracteres");*/
 			return false;
 		}else{
 			//alert("Contraseña segura");
@@ -127,8 +127,8 @@ function validateForm(){
 	contrasena();
 
 	//seleccionar bicicleta
-	/*function seleccionBici(){
-		var bicicleta = $(".select")//document.getElementsByTagName('select'); 
+	function seleccionBici(){
+		var bicicleta = $(".select").val();//document.getElementsByTagName('select'); 
 		for (var i = 0; i < bicicleta.length; i++){ //recorre la lista de opciones
 			if(bicicleta[i].value === "0"){
 			var lista = document.getElementsByClassName('form-group input-box')[1].classList.add('bicicleta'); 
@@ -143,6 +143,6 @@ function validateForm(){
 		} 
 			return true;	
 	}
-	seleccionBici();*/
+	seleccionBici();
 }//function principal
 
